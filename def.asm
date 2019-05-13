e5216b6b2e17618c3ce9b8d5fe64842269a4c99c
Edoboc <50165052+Edoboc@users.noreply.github.com>
2019-05-13 13:16:49 +0200

    Add files via upload

--- def.asm
+++ def.asm
@@ -8,7 +8,8 @@
  .include "macros.asm"
 
 ; === constants definition
- .equ incr = 0b1000
+ .equ incr1 = 0b0100
+ .equ incr0 = 0b0010
  .equ tmax0 = 0b11000000
  .equ tmax1 = 0b00000011		; tmax = 60C
  .equ tmin0 = 0b00100000
@@ -24,6 +25,22 @@
 		ldi r25,tmax1
 		CP2 @1,@0,r25,w
 		breq PC+5
+		CP @2,1
+		breq PC+3
+		ldi w, incr0
+		rjmp PC+2
+		ldi w, incr1
+		add @0,w
+		brcc PC+2
+		inc @1
+		pop w
+	.endmacro
+
+
+.macro 	INCTinf ; incrémente Tinf tant que < Tref (format 2 bytes signé, point fixe à 4)
+		push w
+		CP2 @1,@0,@3,@2
+		breq PC+5
 		ldi w,incr
 		add @0,w
 		brcc PC+2
@@ -36,6 +53,20 @@
 		ldi w,tmin0
 		ldi r25,tmin1
 		CP2 @1,@0,r25,w
+		breq PC+8
+		CP @2,1
+		breq PC+3
+		subi @0, incr0
+		rjmp PC+2
+		subi @0, incr1
+		brcc PC+2
+		dec @1
+		pop w
+	.endmacro
+
+	.macro 	DECTsup ; décrémente Tsup tant que > Tref (format 2 bytes signé, point fixe à 4)
+		push w
+		CP2 @1,@0,@3,@2
 		breq PC+4
 		subi @0, incr
 		brcc PC+2
