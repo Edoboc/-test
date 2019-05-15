148350e28a9e4c66f22abba3f3604416c04ffb93
loicchau <50165245+loicchau@users.noreply.github.com>
2019-05-15 15:41:25 +0200

    Add files via upload

--- Capteurthermo.asm
+++ Capteurthermo.asm
@@ -4,7 +4,7 @@
  *  Created: 15.05.2018 15:37:41
  *   Author: Loïc
  */ 
- .include "def.asm"
+ .include "fonction.asm"
 
  ; === interrupt table
 .org 0
@@ -16,7 +16,6 @@
  reset:	
 			LDSP RAMEND
 			OUTI DDRB,0xff
-			OUTI DDRE,0xff
 			OUTI ADCSR, (1<<ADEN)+(1<<ADIE)+6
 			call ws2812b4_init
 			call ws2812b4_reset		 
@@ -58,12 +57,12 @@ Trefset:
 .db "Set Tref", LF, 0
 			PRINTF LCD
 .db "Tref =",FFRAC2+FSIGN, b, 4, $42, LF, 0
-			LED_COLOR 0x05,0x05,0x05
+			LED_COLOR 0x05,0x05,0x05		; affiche la couleur blanche
 
 Trefinc:
 			in r16, PIND
-			cpi r16, 0b11101111
-			_BRNE Trefdec
+			cpi r16, 0b11101111				; increment Tref, Tsup and Tinf if PD4 is pressed
+			_BRNE Trefdec					; go to Trefdec if not
 			INCT b0,b1,1
 			INCT a0,a1,1
 			INCT a2,a3,1
@@ -71,8 +70,8 @@ Trefinc:
 .db "Tref =",FFRAC2+FSIGN, b, 4, $42, LF, 0
 			WAIT_MS 200
 Trefdec:
-			cpi r16, 0b11110111
-			_BRNE Trefnext
+			cpi r16, 0b11110111				; decrement Tref, Tsup and Tinf if PD3 is pressed
+			_BRNE Trefnext					; go to Trefnext if not
 			DECT b0,b1,1
 			DECT a0,a1,1
 			DECT a2,a3,1
@@ -81,18 +80,18 @@ Trefdec:
 			WAIT_MS 200
 Trefnext:
 			cpi r16, 0b10111111
-			brne PC+3
+			brne PC+3						; go to main if PD6 is pressed
 			rcall LCD_clear
 			rjmp main	
 			cpi r16, 0b11111011
-			brne PC+3
+			brne PC+3						; clear LCD and go to Tableset if PD2 is pressed
 			rcall LCD_clear
 			rjmp Tableset
-			cpi r16, 0b11111101
+			cpi r16, 0b11111101				; clear LCD and go to Tableset if PD1 is pressed
 			brne PC+3
 			rcall LCD_clear
 			rjmp Tableset		
-			rjmp Trefinc
+			rjmp Trefinc					; if any button is pressed, return to Trefinc
 
 Tableset:
 			rcall LCD_home
@@ -105,8 +104,8 @@ Tableset:
 
 Tableinc:
 			in r16, PIND
-			cpi r16, 0b11111011
-			_BRNE Tabledec
+			cpi r16, 0b11111011				; increment Table, Tsup and Tinf if PD2 is pressed
+			_BRNE Tabledec					; go to Tabledec if not
 			INCT b2,b3,1
 			INCT a0,a1,0
 			DECT a2,a3,0
@@ -116,8 +115,8 @@ Tableinc:
 .db "Tinf =",FFRAC2+FSIGN, a+2, 4, $42, LF, 0
 			WAIT_MS 200
 Tabledec:
-			cpi r16, 0b11111101
-			_BRNE Tablenext
+			cpi r16, 0b11111101				; decrement Tref, Tsup and Tinf if PD1 is pressed
+			_BRNE Tablenext					; go to Tablenext if not
 			DECT b2,b3,1
 			DECT a0,a1,0
 			INCT a2,a3,0
@@ -129,36 +128,32 @@ Tabledec:
 			WAIT_MS 200
 Tablenext:
 			cpi r16, 0b10111111
-			brne PC+3
+			brne PC+3						; clear LCD and go to main if PD6 is pressed
 			rcall LCD_clear
 			breq main
 			cpi r16, 0b11101111
-			brne PC+3
+			brne PC+3						; clear LCD and return to Trefset if PD4 is pressed
 			rcall LCD_clear
 			rjmp Trefset
 			cpi r16, 0b11110111
-			brne PC+3
-			rcall LCD_clear
+			brne PC+3						; clear LCD and return to Trefset if PD3 is pressed
+			rcall LCD_clear	
 			rjmp Trefset		
-			rjmp Tableinc
+			rjmp Tableinc					; if any button is pressed, return to Tableinc
 
 main:
-			/*LDSP RAMEND
-			OUTI DDRB,0xff
-			OUTI DDRE,0xff
-			OUTI ADCSR, (1<<ADEN)+(1<<ADIE)+6*/
-			WAIT_MS 200
+			WAIT_MS 200					
 			push a0
-			rcall	lcd_home			; place cursor to home position
-			rcall	wire1_reset			; send a reset pulse
-			CA	wire1_write, skipROM	; skip ROM identification
-			CA	wire1_write, convertT	; initiate temp conversion
-			rcall	wire1_reset			; send a reset pulse
+			rcall	lcd_home				; place cursor to home position
+			rcall	wire1_reset				; send a reset pulse
+			CA	wire1_write, skipROM		; skip ROM identification
+			CA	wire1_write, convertT		; initiate temp conversion
+			rcall	wire1_reset				; send a reset pulse
 			CA	wire1_write, skipROM
 			CA	wire1_write, readScratchpad
-			rcall	wire1_read			; read temperature LSByte
+			rcall	wire1_read				; read temperature LSByte
 			mov	c2,a0
-			rcall	wire1_read			; read temperature MSByte
+			rcall	wire1_read				; read temperature MSByte
 			mov c3,a0
 			pop a0
 			
@@ -167,32 +162,34 @@ main:
 			
 
 			in r16,PIND
-			cpi r16,0b11111110
-			brne PC+2
-			rjmp reset
 			cpi r16,0b10111111
-			brne PC+2
-			rjmp Trefset				
+			brne PC+2						; go to Temp_color if any button pressed
+			rjmp Trefset					; return to Trefset if PD6 is pressed
 
 Temp_color:	
-		PUSH4 a0,a1,a2,a3
+		PUSH4 a0,a1,a2,a3					; save registers
 		PUSH4 b0,b1,c0,c1
-		LDI2 b1,b0, 0x0170
-		SUB2 a1,a0, a3,a2
-		rcall div22
-		MOV2 b1,b0, c1,c0
-		SUB2 c3,c2, a3,a2
-		MOV2 a1,a0, c3,c2
-		rcall div22
-		mov d0,c0
-		_ANDI d0,0xf0
+		LDI2 b1,b0, 0x0170					; load 23 in b1 b0
+		SUB2 a1,a0, a3,a2					; substract Tinf to Tsup, result stored in a1 a0
+		rcall div22							; divide previous result by 23, result stored in c1 c0 (format 2 signed bytes, fix point at 4)
+		MOV2 b1,b0, c1,c0					; move result to b1 b0
+		SUB2 c3,c2, a3,a2					; Tempeture - Tinf
+		MOV2 a1,a0, c3,c2					; move result to a1 a0
+		rcall div22							; previous result/first division result
+		mov d0,c0							; move result to d0 and change format
+		_ANDI d0,0xf0						
 		or d0,c1
-		swap d0
-		POP4 b0,b1,c0,c1
+		swap d0								
+		POP4 b0,b1,c0,c1					; restore rgisters
 		POP4 a0,a1,a2,a3
-		_CPI d0, 0
-		brne PC+20
-		LED_COLOR 0x00,0x00,0x0f	;pure blue							
+		brpl PC+57
+		LED_COLOR 0x00,0x00,0x0f
+		WAIT_MS 300
+		LED_COLOR 0x00,0x00,0x00
+		rjmp main
+		_CPI d0, 0							; compare d0 to various numbers, up to 22
+		brne PC+20							; branch to next comparison if not equal
+		LED_COLOR 0x00,0x00,0x0f			; pure blue							
 		rjmp main
 		_CPI d0, 1
 		brne PC+20
@@ -220,7 +217,7 @@ Temp_color:
 		rjmp main
 		_CPI d0, 7
 		brne PC+20
-		LED_COLOR 0x01,0x0c,0x02	;green-ish
+		LED_COLOR 0x01,0x0c,0x02			; green-ish
 		rjmp main
 		_CPI d0, 8
 		brne PC+20
@@ -236,7 +233,7 @@ Temp_color:
 		rjmp main
 		_CPI d0, 11
 		brne PC+20
-		LED_COLOR 0x05,0x05,0x05	;pure white
+		LED_COLOR 0x05,0x05,0x05			; white
 		rjmp main
 		_CPI d0, 12
 		brne PC+20
@@ -252,7 +249,7 @@ Temp_color:
 		rjmp main
 		_CPI d0, 15
 		brne PC+20
-		LED_COLOR 0x08,0x07,0x00	;yellow
+		LED_COLOR 0x08,0x07,0x00			; yellow
 		rjmp main
 		_CPI d0, 16
 		brne PC+20
@@ -264,7 +261,7 @@ Temp_color:
 		rjmp main
 		_CPI d0, 18
 		brne PC+20
-		LED_COLOR 0x0b,0x04,0x00	;orange
+		LED_COLOR 0x0b,0x04,0x00			; orange
 		rjmp main
 		_CPI d0, 19
 		brne PC+20
@@ -278,16 +275,23 @@ Temp_color:
 		brne PC+20
 		LED_COLOR 0x0e,0x01,0x00
 		rjmp main
-		LED_COLOR 0x0f,0x00,0x00	;pure red
+		_CPI d0, 22
+		brne PC+20
+		LED_COLOR 0x0f,0x00,0x00			; pure red
 		rjmp main
+		LED_COLOR 0x0f,0x00,0x00
+		WAIT_MS 300
+		LED_COLOR 0x00,0x00,0x00
+		rjmp main
+
 
 ws2812b4_reset:
 		cbi PORTE, 1
-		WAIT_US	50 	; 50 us are required, NO smaller works
+		WAIT_US	50 							; 50 us are required, NO smaller works
 		ret					
 
 ws2812b4_init:
-		OUTI	DDRE,0x02
+		OUTI	DDRE,0x02		
 		ret
 ws2812b4_byte3wr:
 
